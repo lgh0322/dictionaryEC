2304e00a0a39013b6a1f5a0dd05468475a6e06eb
李光浩 <liguanghao@viatomtech.com>
2021-04-26 17:54:35 +0800

    地方

--- app/src/main/java/com/vaca/dic/MainActivity.kt
+++ app/src/main/java/com/vaca/dic/MainActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import java.io.InputStream
-
-
-
+import java.nio.charset.StandardCharsets.UTF_16LE
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         val size: Int = dictionaryStream.available()
         val dicByteArray = ByteArray(size)
         dictionaryStream.read(dicByteArray)
-        Log.e("fuck","的实力开发就$size")
+
+        var fuck=dicByteArray.copyOfRange(200,222)
+        val x=String(fuck,UTF_16LE)
+        Log.e("fuck","$x")
     }
+
+
+
 }
