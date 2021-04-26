05a0047cae278e494d264da18893cb52b8e01748
李光浩 <liguanghao@viatomtech.com>
2021-04-26 17:42:14 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/dic/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.vaca.dic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.InputStream
+
+
+#pragma mode( separator(.,;) integer(h32) )
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val dictionaryStream: InputStream = assets.open("Di.fu")
+        val size: Int = dictionaryStream.available()
+        val dicByteArray = ByteArray(size)
+        dictionaryStream.read(dicByteArray)
+        Log.e("fuck","的实力开发就$size")
+    }
+}
