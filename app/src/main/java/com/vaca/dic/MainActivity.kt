23eb3c2442fce3a625e35be241d47c388298956e
李光浩 <liguanghao@viatomtech.com>
2021-04-27 15:32:52 +0800

    好了23

--- app/src/main/java/com/vaca/dic/MainActivity.kt
+++ app/src/main/java/com/vaca/dic/MainActivity.kt
@@ -3,18 +3,45 @@ package com.vaca.dic
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.tencent.bugly.crashreport.CrashReport
 import java.io.InputStream
 import java.nio.charset.StandardCharsets.UTF_16LE
 
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var   appBarConfiguration:AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+       appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.searchFragment,
+            ), drawerLayout
+        )
+       val  mMainNavFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
         //--------------------------------------------------------读取文本， 这个是小的在前面的
         DicTool.dicToolInit(this)
 
@@ -28,7 +55,10 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
 
 
 
---
+++ app/src/main/java/com/vaca/dic/fragment/SearchFragment.kt
@@ -0,0 +1,27 @@
+package com.vaca.dic.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.vaca.dic.R
+
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_about, container, false)
+
+
+        return root
+    }
+
+
+}
